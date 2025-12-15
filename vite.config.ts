0d76bf2278e6ce7a21f0a0e478dcf692c35e268e
hugoldd <hugo.galmiche@gmail.com>
2025-12-15 16:32:48 +0100

    Update base path in Vite config for deployment
    Set base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-
 export default defineConfig({
   plugins: [react()],
-  base: repoName ? `/${repoName}/` : "/",
+  // IMPORTANT : le nom du repo GitHub Pages
+  base: "/ERPV2/",
 });
